eb924e78f498f5243d4c91fad35969632fb22ed1
rkniyer999 <34409227+rkniyer999@users.noreply.github.com>
2018-09-02 13:49:46 +0530

    Add files via upload

--- twiliornd/src/main/java/twiliornd/twiliosms.java
+++ twiliornd/src/main/java/twiliornd/twiliosms.java
@@ -8,13 +8,13 @@ import com.twilio.type.PhoneNumber;
 public class twiliosms {
 
 	  // Find your Account Sid and Token at twilio.com/console
-    public static final String ACCOUNT_SID = "AC17f944594883960e6fd126b36602743d";
-    public static final String AUTH_TOKEN = "f77cd5d1af826d2e60928982885b695e";
+    public static final String ACCOUNT_SID = "ACCOUNT SID";
+    public static final String AUTH_TOKEN = "ACCOUNT TOKEN";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
-                new com.twilio.type.PhoneNumber("+919607042787"),
+                new com.twilio.type.PhoneNumber("+91xxxxxxxxxx"),
                 new com.twilio.type.PhoneNumber("+15055964070"),
                 "Test message from Hemendra....")
             .create();
